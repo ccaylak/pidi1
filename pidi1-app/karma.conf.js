8f8119cd977997b65acb3eef13a99b05e542f50e
Cem Caylak <cem.caylak@adesso.de>
2019-08-20 10:38:57 +0200

    Pact tests

--- pidi1-app/karma.conf.js
+++ pidi1-app/karma.conf.js
@@ -3,30 +3,42 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage-istanbul-reporter"),
+      require("@angular-devkit/build-angular/plugins/karma")
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/pidi1-app'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+      dir: require("path").join(__dirname, "./coverage/pidi1-app"),
+      reports: ["html", "lcovonly", "text-summary"],
+      fixWebpackSourcePaths: true,
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ["progress", "kjhtml"],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ["Chrome"],
     singleRun: false,
     restartOnFileChange: true,
+
+    pact: [{
+      cors: true,
+      port: 1234,
+      consumer: "ui",
+      provider: "apiservice",
+      dir: "pacts",
+      spec: 2,
+    }],
+    proxies: {
+      "/user-service/": "http://127.0.0.1:1234/user/",
+    },
   });
 };
--- pidi1-app/src/app/shared/api.service.pact.spec.ts
+++ pidi1-app/src/app/shared/api.service.pact.spec.ts
@@ -3,3 +3,71 @@ import {HttpClientModule} from '@angular/common/http';
 import {ApiService} from './api.service';
 import {User} from '../user';
 import {PactWeb, Matchers} from '@pact-foundation/pact-web';
+
+describe('ApiService', () => {
+  let provider;
+
+  beforeAll(done => {
+    provider = new PactWeb({
+      consumer: 'ui',
+      provider: 'apiservice',
+      port: 1234,
+      host: '127.0.0.1',
+    });
+    setTimeout(done, 2000);
+    provider.removeInteractions();
+  });
+
+  afterAll(done => {
+    provider.finalize().then(() => {
+      done();
+    }, (err) => {
+      done.fail(err);
+    });
+  });
+
+
+  describe('create()', () => {
+    const expectedUser: User = {
+      firstName: 'Cem',
+      lastName: 'Caylak',
+      birthDate: new Date('1999-01-17'),
+      address: 'Castroper Straße 102',
+      place: 'Datteln',
+      postCode: '45711',
+    };
+
+    beforeAll((done) => {
+      provider.addInteraction({
+        state: `provider accepts a new user`,
+        uponReceiving: 'a request to POST an user',
+        withRequest: {
+          method: 'POST',
+          path: '/user/registration',
+          body: expectedUser,
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        },
+        willRespondWith: {
+          status: 200,
+          body: Matchers.somethingLike({}),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+      }).then(done, error => done.fail(error));
+    });
+
+
+    it('should create an User', done => {
+      const apiService: ApiService = TestBed.get(ApiService);
+      apiService.createUser(expectedUser).subscribe(response => {
+        expect(response).toEqual(expectedUser);
+        done();
+      }, error => {
+        done.fail(error);
+      });
+    });
+  });
+});
